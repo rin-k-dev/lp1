e72372e54ec1f7f1dc68dc7d8c568117a402e348
rink <77056883+rin-k-dev@users.noreply.github.com>
2024-07-02 21:38:09 +0900

    #courseの実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,4 +10,19 @@ $(document).ready(function() {
   $(".menu__link").click(function() {
     $("#menu").removeClass('panelactive').addClass('panelinactive');
   });
+
+  $(function() {
+    // SmartTab initialize
+    $('#smarttab').smartTab({
+      selected: 0, // Initial selected tab, 0 = first tab
+      theme: 'elite', // theme, related css need to include for other than default theme
+      justified: true, // Nav menu justification. true/false
+      autoAdjustHeight: true, // Automatically adjust content height
+      backButtonSupport: true, // Enable the back button support
+      enableUrlHash: true, // Enable selection of the step based on url hash
+      transition: {
+        animation: 'none', // Animation effect on navigation, none|fade|slideHorizontal|slideVertical|slideSwing|css(Animation CSS class also need to specify)
+      },
+    });
+  });
 });
